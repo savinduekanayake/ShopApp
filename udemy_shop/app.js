ae542eded0ffa4b99c70e112baa5b53b1b5ee5d5
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2019-12-15 23:55:19 +0530

    html file to pug(82)

--- udemy_shop/app.js
+++ udemy_shop/app.js
@@ -21,7 +21,7 @@ app.use(shopRoutes);
 
 // not a regular page
 app.use('/',(req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404');
 });
 
 
