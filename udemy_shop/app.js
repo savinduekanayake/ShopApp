366fe784a7d9f5cf59d0d890bd393b69769845e2
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2019-12-18 14:35:13 +0530

    controller finished

--- udemy_shop/app.js
+++ udemy_shop/app.js
@@ -3,35 +3,26 @@ const bodyPaser = require('body-parser');
 const path = require('path');
 const parser = require('parse')
 
-//import handleBar template
-//const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error')
 
 const app = express();
 
-//for handle bar you need to implement app.engine
-//app.engine('hbs',expressHbs({layoutsDir: 'views/layouts',defaultLayout:'main-layout', extname:'hbs'}))
 
-// dynamic template
-//app.set('view engine','hbs');
-//app.set('view engine','pug');
-//
 app.set('view engine', 'ejs')
 app.set('views','views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyPaser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/admin',adminData.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
 // not a regular page
-app.use('/',(req,res,next)=>{
-    res.status(404).render('404',{layout:false, pageTitle:'Page not found'});
-});
+app.use('/',errorController.get404);
 
 //layout:false need when handlebar use. otherwise occuring errors
 
--- udemy_shop/routes/shop.js
+++ udemy_shop/routes/shop.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productController = require('../controllers/products')
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    console.log(products)
-    console.log(products.length)
-    res.render('shop', 
-    { //layout: false, 
-        prods: products, 
-        pageTitle: 'Shop', path: '/', 
-        hasProducts: products.length > 0 ,
-        activeShop:true,
-        productCSS:true
-    })
-});
+router.get('/', productController.getProducts );
 
 module.exports = router;
