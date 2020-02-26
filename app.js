7ca29e36305fec51ac44afd4a03f85007856e163
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-02-27 00:09:26 +0530

    started with cockies

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.set('views','views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 
 app.use(bodyPaser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')))
@@ -33,6 +35,8 @@ app.use((req,res,next)=>{
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
 
 
 // not a regular page
@@ -45,7 +49,8 @@ app.use('/',errorController.get404);
 // })
 
 mongoose
-    .connect('mongodb://savindu:yzQHPrSRuimRVRW6@cluster0-shard-00-00-r106v.mongodb.net:27017,cluster0-shard-00-01-r106v.mongodb.net:27017,cluster0-shard-00-02-r106v.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+    .connect('mongodb://savindu:yzQHPrSRuimRVRW6@cluster0-shard-00-00-r106v.mongodb.net:27017,cluster0-shard-00-01-r106v.mongodb.net:27017,cluster0-shard-00-02-r106v.mongodb.net:27017/shop?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    {useUnifiedTopology: true,useNewUrlParser: true,})
     .then((result)=>{
         User.findOne().then(user=>{
             if(!user){
@@ -59,7 +64,7 @@ mongoose
                 user.save();
             }
         })
-        
+        console.log('CONNECTED!');
         app.listen(3000);
     }).catch(err=>{
         console.log(err);
