436ab465c69d48b5d7f1184c20e981d8468eb294
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-05 01:07:21 +0530

    protected routes from unautheenticated access

--- routes/admin.js
+++ routes/admin.js
@@ -7,24 +7,24 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
+const isAuth  = require('../middleware/is-auth');
 
 //   /admin/add-product =>GET
-router.get('/add-product',adminController.getAddProduct);
+router.get('/add-product',isAuth,adminController.getAddProduct);
 
 //   /admin/products =>GET
-router.get('/products',adminController.getProducts);
+router.get('/products',isAuth,adminController.getProducts);
 
 //   /admin/add-product =>POST
-router.post('/add-product',adminController.postAddProduct);
+router.post('/add-product',isAuth,adminController.postAddProduct);
 
-router.get('/edit-product/:productId',adminController.getEditProduct);
+router.get('/edit-product/:productId',isAuth,adminController.getEditProduct);
 
 //  /admin/edit-product =>POST
-router.post('/edit-product',adminController.postEditProduct);
+router.post('/edit-product',isAuth,adminController.postEditProduct);
 
 // //delete a product 
-router.post('/delete-product',adminController.postDeleteProduct)
+router.post('/delete-product',isAuth,adminController.postDeleteProduct)
 
 // // exports.routes = router;
 // // exports.products = products;
