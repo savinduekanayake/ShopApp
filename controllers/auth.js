723696f94ffb28a6113324a8ffe3b18baec8e900
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-09 22:49:37 +0530

    implimented token logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
-const User = require('../models/user');
-const nodemailer = require('nodemailer');
-const sendgridTransport = require('nodemailer-sendgrid-transport');
+const crypto = require('crypto');
 
+const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+
+const nodemailer = require('nodemailer');  // sending mails
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -119,4 +121,57 @@ exports.postLogout = (req, res, next) => {
         console.log(err);
         res.redirect('/');
     })
+}
+
+exports.getReset = (req,res,next)=>{
+    let message = req.flash('error') //getting from session. then remove
+    if(message.length>0){
+        message= message[0];
+    }else{
+        message=null;
+    }
+
+    res.render('auth/reset', {
+        path: '/reset',
+        pageTitle: 'Reset Password',
+        // isAuthenticated: false
+        errorMessage:message
+    });
+}
+
+exports.postReset =(req,res,next)=>{
+    crypto.randomBytes(32,(err,buffer)=>{
+        if(err){
+            console.log(err)
+            return res.redirect('/reset')
+        }
+        const token = buffer.toString('hex');
+        User.findOne({email:req.body.email})
+            .then(user=>{
+                if(!user){
+                    req.flash('err', "No account with that email found!");
+                    return res.redirect('/reset');
+                }
+                //set token to user
+                user.resetToken=token;
+                user.resetTokenExpiration = Date.now() + 3600000;
+                return user.save()
+            })
+            .then(result=>{
+                res.redirect('/');
+                
+                //sending mail
+                transporter.sendMail({
+                    to:req.body.email,
+                    from: 'shop@node.com',
+                    subject: 'Password reset',
+                    html: `
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="http://localhost:3000/reset/${token}"> link to set a new password.</P>
+
+                    `
+                })
+            })
+            .catch(err=>console.log(err))
+    });
 }
