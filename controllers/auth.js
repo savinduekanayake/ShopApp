e7df00e3a0a82d8950f7c5aacd89f44441fe8c56
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-14 12:17:47 +0530

    validation start

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
 const crypto = require('crypto');
-const bcrypt = require('bcryptjs');
 
+const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');  // sending mails
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check')
 
 const User = require('../models/user');
 
-
-const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
@@ -87,6 +87,17 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors  = validationResult(req);
+
+    if(!errors.isEmpty()){
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            // isAuthenticated: false
+            errorMessage:errors.array()[0].msg
+        });
+    }
 
     User.findOne({ email: email })
         .then(userDoc => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -10,7 +11,26 @@ router.post('/login',authController.postLogin);
 
 router.get('/signup',authController.getSignup);
 
-router.post('/signup',authController.postSignup);
+router.post(
+    '/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.') // change the message
+            .custom((value,{ req }) =>{     //customize the validation with our own
+                if(value === 'test@test.com'){
+                    throw new Error('This email address is forbidden');
+                    
+                }
+                return true;
+            }),
+        body('password',    // check password value in body./we can use check also. 
+             'Please enter a password with only numbers and text and at least 5 characters'  //error inteted of .withMessage()
+            )    
+             .isLength({min:5})
+             .isAlphanumeric()   
+    ],
+        authController.postSignup);
 
 router.post('/logout',authController.postLogout);
 
